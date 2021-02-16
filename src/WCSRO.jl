391de8779643848cb594154659dd9b889aa0f01c
Ulrich Pototschnig <ulrich.pototschnig@hotmail.com>
2021-02-16 09:54:22 +0100

    Reworked execution sheet

--- src/WCSRO.jl
+++ src/WCSRO.jl
@@ -1,7 +1,6 @@
-module WCSRO
-include("structs.jl")
+include("structure.jl")
 include("initialize.jl")
-include("atom_shuffle.jl")
+include("shuffle_config.jl")
 include("sro_calc.jl")
 include("plotting.jl")
 
@@ -13,39 +12,42 @@ different elements. This file shall guide you through the necessary steps.
 """
 
 # INPUT
-# The input can either be of type '.yaml' or a POSCAR-file:
-file = "ab.yml"
+# Please declare your input YAML-file:
+
+file = "testfile.yaml"
 
 # This will create all structures for a SRO calculation and stored it in an
 # object of type 'Backpack'.
+
 my_backpack = initialize(file)
 
+# Now, the SRO parameters are being calculated while the atomic configuration is
+# shuffled in between. The input number below determines the number of
+# non-repetitive shuffles:
 
-#Now, the SRO parameters are being calculated while the atomic configuration is
-#shuffled in between. The input number below determines the number of
-#non-repetitive shuffles:
 shuffles = 100
-unique_perms = shuffle_atoms(my_backpack, shuffles)
-@time shuffle_atoms(my_backpack, shuffles)
-unique_perms
+perms = shuffle_atoms(my_backpack, shuffles)
+#xyz_animation(my_backpack, unique_perms[2])
 
-
-#Alternatively, if you want to generate ALL permutations, you can use this function
-#below. You find the documentations for this functions in 'atom_shuffle.jl' as well.
-#Please keep in mind that this can be very CPU-intensive and is therefore not
-#suitable for too large systems. But feel free to try it out. ;-)
+# Alternatively, if you want to generate aLL permutations, you can use this function
+# below. You find the documentations for this functions in 'shuffle_sonfig.jl' as well.
+# Please keep in mind that this can be very CPU-intensive and is therefore not
+# suitable for too large systems. But feel free to try it out. ;-)
 # @time begin
-    # all_perms = all_permutations(sequence(my_backpack))
+    # perms = all_permutations(sequence(my_backpack))
 # end
-# println(size(all_perms))
 
 
-#Now, we create all corresponding SRO values, which are simple sums of pairs and
-#triplets without any weighing factors. So please keep in mind that these values
-#are only qualitative measurements for order.
+# Now, we start SRO calculation. With the function 'evaluate', SROs are calculated for
+# all created permutations and the minima and maxima for pairs, triplets and a
+# sum of both are returned separately. The value in the function is the triplet weight factor,
+# which defines the contribution of the triplets
+results = evaluate(my_backpack, perms, 0.2)
 
-sro_list = []
-for i = 1:size(unique_perms)[1]
-    push!(sro_list, @time sro(my_backpack, unique_perms[i, :]))
-end
-end
+# Finally, see what you did! :-)
+# You can either plot some reults, like here the pairs...
+plot_configs(results.pairs)
+#...or you plot the whole structure, for example the minimum for triplets...
+#xyzplot(my_backpack, perms[results.triplet_extrema[1][2]], 15.0)
+#...you can even create a little gif, if you want:
+#xyz_animation(my_backpack, perms[results.triplet_extrema[1][2]])
